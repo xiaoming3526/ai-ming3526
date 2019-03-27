c4fa0b08d59e7c43e329452bc62cff1c898db037
xiaoming3526 <272273581@qq.com>
2019-03-27 21:42:12 +0800

    KNN数据集

--- blog/src/py2.x/ml/2.KNN/KNN.py
+++ blog/src/py2.x/ml/2.KNN/KNN.py
@@ -19,6 +19,8 @@ import operator
 # 导入科学计算包numpy和运算符模块operator
 from os import listdir
 from collections import Counter
+import matplotlib
+import matplotlib.pyplot as plt
 
 def createDataSet():
     """
@@ -174,7 +176,45 @@ def classify1(inX, dataSet, labels, K):
     label = Counter(k_labels).most_common(1)[0][0]
     return label
 
+def file2matrix(filename):
+   """
+   Desc:
+       导入训练数据
+   parameters:
+       filename: 数据文件路径
+   return: 
+       数据矩阵 returnMat 和对应的类别 classLabelVector
+   """
+   fr = open(filename)
+   # 获得文件中的数据行的行数
+   numberOfLines = len(fr.readlines())
+   # 生成对应的空矩阵
+   # 例如：zeros(2，3)就是生成一个 2*3的矩阵，各个位置上全是 0 
+   returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+   classLabelVector = []  # prepare labels return
+   fr = open(filename)
+   index = 0
+   for line in fr.readlines():
+       # str.strip([chars]) --返回已移除字符串头尾指定字符所生成的新字符串
+       line = line.strip()
+       # 以 '\t' 切割字符串
+       listFromLine = line.split('\t')
+       # 每列的属性数据
+       returnMat[index, :] = listFromLine[0:3]
+       # 每列的类别数据，就是 label 标签数据
+       classLabelVector.append(int(listFromLine[-1]))
+       index += 1
+   # 返回数据矩阵returnMat和对应的类别classLabelVector
+   return returnMat, classLabelVector
+
 if __name__ == '__main__':
-    test1()
+    #test1()
+    '''
+    datingDataMat, datingLabels = file2matrix('C:/Users/Administrator/Desktop/blog/github/ai-ming3526/data/2.KNN/datingTestSet2.txt')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+    plt.show()
+    '''
     
 
