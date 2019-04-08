c3b81e90ee7c3e2fcdb9d7e451ad2bdca4b883c7
xiaoming3526 <272273581@qq.com>
2019-04-08 16:19:35 +0800

    knn-demo02识别手写字体

--- blog/src/py2.x/ml/2.KNN/KNN_demo02.py
+++ blog/src/py2.x/ml/2.KNN/KNN_demo02.py
@@ -17,6 +17,7 @@ from numpy import *
 import operator
 from os import listdir
 from collections import Counter
+import KNN
 
 def img2vecor(filename):
     """
@@ -32,7 +33,6 @@ def img2vecor(filename):
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
-        print(lineStr)
         for j in range(32):
             returnVector[0, 32*i+j] = int(lineStr[j])
     return returnVector
@@ -43,6 +43,7 @@ def handwritingClassTest():
     trainingFileList  = listdir('data/2.KNN/trainingDigits')
     m = len(trainingFileList)
     trainingMat = zeros((m,1024))
+    # hwLabels存储0～9对应的index位置， trainingMat存放的每个位置对应的图片向量
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]
@@ -50,9 +51,22 @@ def handwritingClassTest():
         hwLabels.append(classNumStr)
         # 将 32*32的矩阵->1*1024的矩阵
         trainingMat[i, :] = img2vecor('data/2.KNN/trainingDigits/%s' % fileNameStr)
-        print(hwLabels)
-        print(trainingMat)
 
+    # 2. 导入测试数据
+    testFileList = listdir('data/2.KNN/testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest =  len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vecor('data/2.KNN/testDigits/%s' % fileNameStr)
+        classifierResult = KNN.classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): 
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
     
 if __name__ == '__main__':
     #打开的文件名 自己修改文件路径
